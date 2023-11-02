1d9c8dafba18fd1d8bd87533f29852159fa00675
nguyen quang <nguyenquangos0302git@gmail.com>
2023-11-02 20:56:27 +0700

    update

--- authorization/src/main/java/com/oauth2/authorization/entity/User.java
+++ authorization/src/main/java/com/oauth2/authorization/entity/User.java
@@ -21,6 +21,6 @@ public class User {
     private String password;
 
     private String role;
-    private boolean enabled = false;
+//    private boolean enabled = false;
 
 }
--- client/src/main/java/com/oauth2/client/config/WebSecurityConfig.java
+++ client/src/main/java/com/oauth2/client/config/WebSecurityConfig.java
@@ -1,6 +1,10 @@
 package com.oauth2.client.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 public class WebSecurityConfig {
@@ -11,5 +15,23 @@ public class WebSecurityConfig {
             "/verifyRegistration*",
             "/resendVerifyToken*"
     };
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors()
+                .and()
+                .csrf()
+                .disable()
+                .authorizeHttpRequests()
+                .antMatchers(WHITE_LIST_URLS).permitAll()
+                .antMatchers("/api/**").authenticated()
+                .and()
+                .oauth2Login(oauth2login ->
+                        oauth2login.loginPage("/oauth2/authorization/api-client-oidc"))
+                .oauth2Client(Customizer.withDefaults());
+
+        return http.build();
+    }
 
 }
